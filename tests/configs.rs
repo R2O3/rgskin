c83db11daeed27edb2afb9033557912e0423ae77
menvae <menvaedev@gmail.com>
2026-01-07 04:05:16 +0200

    fix test

--- tests/configs.rs
+++ tests/configs.rs
@@ -10,7 +10,7 @@ fn osu_skin_ini_test() -> Result<(), Box<dyn std::error::Error>> {
     benchmark_closure(||
     {
         let raw_skin_ini = read_file_to_string("./tests/configs/osu/bubbleskin.ini").unwrap();
-        let _skin_ini = SkinIni::from_str(&raw_skin_ini)?;
+        let _skin_ini = OsuSkinIni::from_str(&raw_skin_ini)?;
         Ok(())
     }, "Osu! skin ini", "to finish skin ini parsing", "\x1b[0;33m")?;
     Ok(())
